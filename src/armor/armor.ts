8bcf157b906d719470533f7d4de8159b57f2856b
Israel Gilyadov <israelg99@gmail.com>
2017-02-23 00:24:22 +0200

    Engineer armor using equippable

--- src/armor/armor.ts
+++ src/armor/armor.ts
@@ -1,8 +1,12 @@
+import { Player } from './../player/player';
 import { RNG } from '../util/rng';
 import { Plastic } from './plastic';
 import { preload_dict } from '../util/preload';
+import { Equippable, replace, drop, loot } from '../items/equippable';
 
-export abstract class Armor {
+export abstract class Armor implements Equippable {
+    public readonly dropRate: number = 0.5
+
     protected health: number
     protected armor: number
 
@@ -24,5 +28,23 @@ export abstract class Armor {
 
     public mitigate(damage: number): number {
         return Math.max(0, damage-this.mitigation(damage))
+    }
+
+    public replace(player: Player): void {
+        replace(this, player.armor, player)
+    }
+    public drop(player: Player): void {
+        drop(this, player)
+    }
+    public loot(player: Player): void {
+        loot(this, player)
+    }
+    public equip(player: Player): void {
+        player.armor = this
+    }
+    public unEquip(player: Player): Equippable {
+        let armor: Armor = player.armor
+        player.armor = undefined
+        return armor
     }
 }
