b29058dfa39ffceead3e4c0868d960c27d238de7
Israel Gilyadov <israelg99@gmail.com>
2017-02-22 19:44:28 +0200

    Improve Item to be class agnostic

--- src/items/item.ts
+++ src/items/item.ts
@@ -5,19 +5,20 @@ import { Weapon } from '../weapons/weapon';
 export class Item extends Phaser.Sprite {
     private readonly item: Weapon
 
-    constructor(item: Weapon, x: number, y: number) {
-        super(phaser, x, y, item.key + '_d')
+    constructor(item: any, x: number, y: number, key: string = item.constructor.name.toLowerCase()) {
+        console.log(key)
+        super(phaser, x, y, key + '_d')
         phaser.physics.enable(this, Phaser.Physics.ARCADE)
         this.anchor.setTo(0.25, -0.5)
         this.item = item
     }
 
-    public pickup(): Weapon {
+    public pickup(): any {
         this.destroy(true)
         return this.item
     }
 
-    public static drop(sprite: Weapon, x?: number, y?: number): void {
+    public static drop(sprite: any, x?: number, y?: number): void {
         let tx: number = x ? x : sprite.x
         let ty: number = y ? y : sprite.y
         items.add(new Item(sprite, tx, ty))
